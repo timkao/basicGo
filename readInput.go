f96ad3dd5373c1f66b423a99606352e9b08df99e
Tim Kao <pakuya.kao@gmail.com>
2018-10-08 20:57:17 -0400

    buffer io reader

--- readInput.go
+++ readInput.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 var (
 	firstName, lastName, s string
@@ -12,10 +16,19 @@ var (
 
 func main() {
 
+	/* use scanf */
 	fmt.Println("Please enter your full name: ")
 	fmt.Scanln(&firstName, &lastName)
 	fmt.Printf("Hi %s %s\n", firstName, lastName)
 	fmt.Sscanf(input, format, &f, &i, &s)
 	fmt.Println("From the string we read: ", f, i, s)
+
+	/* use os.Stdin */
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter some input: ")
+	input, err := inputReader.ReadString('\n')
+	if err == nil {
+		fmt.Printf("The input was: %s\n", input)
+	}
 
 }
