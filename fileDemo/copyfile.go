150afde6e0c8b8d0dd9c72c95a10762ca2aabcc0
Tim Kao <pakuya.kao@gmail.com>
2018-10-09 21:39:12 -0400

    copy to a new file

---
+++ fileDemo/copyfile.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	copyFile("target.txt", "source.txt")
+	fmt.Println("file is copied")
+}
+
+func copyFile(dst string, src string) (written int64, err error) {
+	srcFile, err1 := os.Open(src)
+	if err1 != nil {
+		fmt.Println("fail to open", src)
+		return
+	}
+	defer srcFile.Close()
+
+	dstFile, err2 := os.Create(dst)
+	if err2 != nil {
+		fmt.Println("fail to open", dst)
+		return
+	}
+	defer dstFile.Close()
+
+	return io.Copy(dstFile, srcFile)
+
+}
