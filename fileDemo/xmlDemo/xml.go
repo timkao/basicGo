b9be7241bf7c10a20c70bd880200db8ad55019c3
Tim Kao <pakuya.kao@gmail.com>
2018-10-10 21:43:10 -0400

    read xml format data

---
+++ fileDemo/xmlDemo/xml.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	input := "<person><FirstName>Tim</FirstName><LastName>Kao</LastName></person>"
+	inputReader := strings.NewReader(input)
+	p := xml.NewDecoder(inputReader)
+
+	for t, err := p.Token(); err == nil; t, err = p.Token() {
+		switch token := t.(type) {
+		case xml.StartElement:
+			name := token.Name.Local
+			fmt.Printf("Token name: %s\n", name)
+		case xml.EndElement:
+			fmt.Println("end of token")
+		case xml.CharData:
+			content := string([]byte(token)) // turn token to []byte then to string
+			fmt.Printf("this is the content %s\n", content)
+		default:
+			fmt.Println("cannt read it")
+		}
+	}
+}
