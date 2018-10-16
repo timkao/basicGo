c8bac45537e5c88424af9fb553fc03b4fef4c256
Tim Kao <pakuya.kao@gmail.com>
2018-10-15 21:38:42 -0400

    panicing and recover

---
+++ panicRecover.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func badCall() {
+	panic("this is a bad call")
+}
+
+func test() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("panicing %s\n", e)
+		}
+	}()
+	badCall()
+	fmt.Println("After bad call")
+}
+
+func main() {
+	fmt.Println("calling test")
+	test()
+	fmt.Println("complete!")
+}
