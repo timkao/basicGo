19ab35b506951c3679421d41e0b90c3a9d214fe7
Tim Kao <pakuya.kao@gmail.com>
2018-10-17 20:55:06 -0400

    channel block or not

---
+++ channelBlock.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+	对于同一个通道，发送操作（协程或者函数中的），在接收者准备好之前是阻塞的
+*/
+func main() {
+	ch1 := make(chan int)
+
+	go pump(ch1)
+	fmt.Println(<-ch1)
+}
+
+func pump(ch chan int) {
+	for i := 0; ; i++ {
+		ch <- i
+	}
+}
--- goChannelDemo.go
+++ goChannelDemo.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	ch := make(chan string)
+
+	// 如果 2 个协程需要通信，你必须给他们同一个通道作为参数才行。
 	go sendData(ch)
 	go getData(ch)
 
+	// main() 等待了 1 秒让两个协程完成，如果不这样，sendData() 就没有机会输出。
 	time.Sleep(1e9)
 }
 
@@ -26,6 +29,8 @@ func sendData(ch chan string) {
 */
 func getData(ch chan string) {
 	var input string
+	// 注意：不要使用打印状态来表明通道的发送和接收顺序：由于打印状态和通道实际发生读写的时间延迟会导致和真实发生的顺序不同。
+
 	for {
 		input = <-ch
 		fmt.Printf("%s, \n", input)
